3a1d6f8ae5a20825798dbcc834cd4bdc4da502e6
Firdavs Mirzaev <fmirzaev@yahoo.com>
2021-06-22 22:03:28 -0400

    name your commitment

---
+++ Utility.java
@@ -0,0 +1,30 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.io.*;
+import java.lang.ProcessBuilder.Redirect;
+import java.nio.file.FileSystemNotFoundException;
+import java.util.*;
+
+public class Utility {
+	
+	
+	/*
+	 * Implement a static utility method that reads from a .txt file and returns the information in the file
+	 * as a List of String [], where each element (i.e String[] ) represents each line in the file
+	 * which contains all the required information about each customer (name, ssn, accountType, initialBalance)
+	 * 
+	 * To read from a file use Scanner class and nextLine() method
+	 * 
+	 * Use try-catch block to handle the FileNotFoundException
+	 *
+	 * Optionally use try-with-resources statement to auto-close the Scanner object 
+	 */
+	public static List<String[]> readFromAFile(String customers) throws IOException {
+		
+
+
+	}}
+	
+	
+
+
